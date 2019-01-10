382c66004c8caed0ec553af8ee6999c4ff10de27
Ankush Sharma <ankush.sharma_cs16@gla.ac.in>
2019-01-10 07:48:47 -0800

    Create Intro to Tutorial Challanges.c

---
+++ Sorting domain solutions/Intro to Tutorial Challanges.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int v,n,i;
+    scanf("%d",&v);
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==v)
+            printf("%d",i);
+    }
+    return 0;
+}
